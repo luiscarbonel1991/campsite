970a95f3c37a6e05fa844a813a4a55248fa49dbb
Luis Carbonel <luis.carbonel@despegar.com>
2023-01-18 00:04:23 -0300

    Upgrade jib plugin version from 3.3.0 to 3.3.1. Also remove some dependencies.

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 	id("org.springframework.boot") version "3.0.1"
 	id("io.spring.dependency-management") version "1.1.0"
 	id("io.freefair.lombok") version "6.6.1"
-	id("com.google.cloud.tools.jib") version "3.3.0"
+	id("com.google.cloud.tools.jib") version "3.3.1"
 }
 
 group = "com.reservation"
@@ -11,7 +11,7 @@ version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_17
 
 val springDocVersion = "2.0.2"
-val shedlockVersion = "4.28.0"
+val commonsValidatorVersion = "1.7"
 
 repositories {
 	mavenCentral()
@@ -21,15 +21,13 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("commons-validator:commons-validator:1.7")
+	implementation("commons-validator:commons-validator:$commonsValidatorVersion")
 	implementation("org.hibernate.validator:hibernate-validator")
 
 	// for lock registry
 	implementation ("org.springframework.boot:spring-boot-starter-data-redis")
 	implementation ("org.springframework.integration:spring-integration-redis")
 	implementation ("io.lettuce:lettuce-core")
-	implementation("net.javacrumbs.shedlock:shedlock-spring:$shedlockVersion")
-	implementation("net.javacrumbs.shedlock:shedlock-provider-jdbc-template:$shedlockVersion")
 
 	// swagger
 	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:$springDocVersion")
@@ -60,6 +58,5 @@ jib {
 	}
 	container {
 		jvmFlags = listOf("-Xms500m", "-Xmx500m")
-		creationTime = "USE_CURRENT_TIMESTAMP"
 	}
 }
